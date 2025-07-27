80b2a49135b4e4e528c0d7fc41f050f5d4f82fd7
chanih <ch0527119706@gmail.com>
2025-07-27 14:52:25 +0300

    chani commit 27/07

--- Javascipt/cart.js
+++ Javascipt/cart.js
@@ -19,7 +19,6 @@ function addToCart(button) {
 
 }
 document.addEventListener('DOMContentLoaded', function () {
-    // שם תעדכני את totalPrice
     const total = cart.reduce((sum, product) => sum + product.price, 0);
     document.getElementById('totalPrice').innerText = "סה״כ לתשלום: " + total + " ₪"
 });
@@ -69,6 +68,64 @@ function deleteItem(itemId) {
     });
 
 }
+document.getElementById("checkout-btn").addEventListener("click", ToPay);
+
+function ToPay() {
+  if (document.getElementById("popup")) {
+    document.getElementById("popup").classList.remove("hidden");
+    return;
+  }
+
+  const Pay = document.createElement('div');
+  Pay.innerHTML = `
+    <div id="popup">
+      <div class="popup-content">
+        <span id="close-btn">&times;</span>
+        <div id="popup-body">
+          <form id="payment-form">
+            <h2>פרטי תשלום</h2>
+            <label>שם מלא:</label>
+            <input type="text" placeholder="ישראל ישראלי" required>
+            <label>מספר כרטיס:</label>
+            <input type="text" placeholder="1234 5678 9012 3456" required>
+            <label>תוקף:</label>
+            <input type="text" placeholder="MM/YY" required>
+            <label>CVV:</label>
+            <input type="text" placeholder="123" required>
+            <label>אימייל:</label>
+            <input type="email" placeholder="example@mail.com" required>
+            <button type="submit" >שלם עכשיו</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  `;
+  document.body.appendChild(Pay);
+
+  // ← לא צריך DOMContentLoaded כי את יוצרת את האלמנטים עכשיו
+
+  // סוגר את הפופאפ בלחיצה על X
+  document.getElementById("close-btn").addEventListener("click", () => {
+    document.getElementById("popup").classList.add("hidden");
+  });
+
+  // סוגר בלחיצה על הרקע
+  document.getElementById("popup").addEventListener("click", (e) => {
+    if (e.target.id === "popup") {
+      document.getElementById("popup").classList.add("hidden");
+    }
+  });
+
+  // שליחת הטופס
+  document.getElementById("payment-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    alert("התשלום התקבל בהצלחה!");
+    window.location.href = "thanks.html"; // מעבר לדף אחר
+    document.getElementById("popup").classList.add("hidden");
+  });
+
+}
+
 
 
 
